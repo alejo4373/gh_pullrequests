e1248e1cde8e063264d5f3aa40c2f85435664380
Alejo <alejandrofranco@pursuit.org>
2019-08-06 12:56:18 -0400

    forks with name and url + cloning them to local

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
-const auth = require('./secrets')
-const API_URL = 'https://api.github.com' 
+const path = require('path');
+const git = require('nodegit');
+const auth = require('./secrets');
+const API_URL = 'https://api.github.com';
 
 const getForks = async (repo_url) => {
   try {
@@ -13,7 +15,32 @@ const getForks = async (repo_url) => {
       },
       auth
     })
-    return res.data;
+
+    let forks = res.data.map(fork => { 
+      return {
+        clone_url: fork.clone_url,
+        full_name: fork.full_name
+       }
+    })
+
+    return forks
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const cloneRepos = async (repos) => {
+  try {
+    let clonePromises = []
+    for(let repo of repos) {
+      // a repo with full name: user/repo will become user@repo for valid 
+      // directory naming purposes
+      const repoName = repo.full_name.replace(/\//, '@'); 
+      const localPath = path.resolve(__dirname, 'forks', repoName);
+      clonePromises.push(git.Clone(repo.clone_url, localPath))
+    }
+    let clonedRepos = await Promise.all(clonePromises)
+    return clonedRepos;
   } catch (err) {
     console.error(err)
   }
@@ -22,8 +49,9 @@ const getForks = async (repo_url) => {
 const main = async () => {
   try {
     let forks = await getForks('/repos/alejo4373/6_2_class_names/forks')
-    let forksUrls = forks.map(fork => fork.clone_url)
-    console.log(forksUrls)
+    console.log(forks.length)
+    let clonedRepos = await cloneRepos(forks)
+    console.log('clonedRepos =>', clonedRepos.length)
   } catch (err) {
     throw err;
   }
