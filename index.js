5c27fbabef675079a04c8ee6520560256420c62c
Alejo <alejandrofranco@pursuit.org>
2019-08-06 11:36:58 -0400

    outsorce getForks function

--- index.js
+++ index.js
@@ -2,18 +2,28 @@ const axios = require('axios');
 const auth = require('./secrets')
 const API_URL = 'https://api.github.com' 
 
-const main = async () => {
+const getForks = async (repo_url) => {
   try {
     const res = await axios({
       method: 'get',
       baseURL: API_URL,
-      url: '/repos/alejo4373/6_2_class_names/forks',
+      url: repo_url,
       params: {
         per_page: '100'
       },
       auth
     })
-    console.log('res =>', res.data.length)
+    return res.data;
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const main = async () => {
+  try {
+    let forks = await getForks('/repos/alejo4373/6_2_class_names/forks')
+    let forksUrls = forks.map(fork => fork.clone_url)
+    console.log(forksUrls)
   } catch (err) {
     throw err;
   }
