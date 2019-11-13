cf3bea05f5a052ca408fd50160a9fc938449516f
Alejo <alejandrofranco@pursuit.org>
2019-11-13 13:44:38 -0500

    add --forks-from & --repos-file CLI options

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const getForks = async (repo_url) => {
       auth
     })
 
-    let forks = res.data.map(fork => { 
+    let forks = res.data.map(fork => {
       return {
         clone_url: fork.clone_url,
         ownerLogin: fork.owner.login,
-       }
+      }
     })
 
     return forks
@@ -33,7 +33,7 @@ const getForks = async (repo_url) => {
 const cloneRepos = async (repos, destPath) => {
   try {
     let clonePromises = []
-    for(let repo of repos) {
+    for (let repo of repos) {
       const repoLocalPath = path.resolve(destPath, repo.ownerLogin);
       clonePromises.push(git.Clone(repo.clone_url, repoLocalPath))
     }
@@ -50,7 +50,7 @@ const handleDestFolder = async (dest) => {
     await fsPromises.mkdir(destPath, { recursive: true });
     console.log("Created folder: " + destPath)
     return destPath;
-  } catch(err) {
+  } catch (err) {
     if (err.code === "EEXIST") {
       return destPath
     } else {
@@ -59,26 +59,55 @@ const handleDestFolder = async (dest) => {
   }
 }
 
+const handleReposFile = async (reposFile) => {
+  try {
+    const filePath = path.resolve(__dirname, reposFile)
+    let file = await fsPromises.readFile(filePath, 'utf-8')
+    let bareRepos = file.split('\n');
+    let repos = []
+    for (let repo of bareRepos) {
+      if (repo)
+        repos.push({
+          ownerLogin: repo.split('/')[0],
+          clone_url: `https://github.com/${repo}.git`
+        })
+    }
+    return repos;
+  } catch (err) {
+    throw (err)
+  }
+}
+
 const main = async () => {
-  const repo = process.argv[2];
+  const repoArg = process.argv[2];
   const dest = process.argv[3];
 
   if (!dest) {
     throw new Error('No destination folder specified');
   }
 
-  const destPath = await handleDestFolder(dest);
-
-  if (!repo) {
-    throw new Error('No repo name specified')
+  if (!repoArg) {
+    throw new Error('No repo name or --repos-file=repos.txt option specified')
   }
-  
-  try {
-    let forks = await getForks(`/repos/${repo}/forks`)
-    console.log(forks.length)
 
-    let clonedRepos = await cloneRepos(forks, destPath)
-    console.log('clonedRepos =>', clonedRepos.length)
+  const destPath = await handleDestFolder(dest);
+  const [option, optionValue] = repoArg.split('=')
+  let clonedRepos;
+  try {
+    switch (option) {
+      case "--repos-file":
+        const repos = await handleReposFile(optionValue)
+        clonedRepos = await cloneRepos(repos, destPath)
+        break;
+      case "--forks-from":
+        const forks = await getForks(`/repos/${optionValue}/forks`)
+        clonedRepos = await cloneRepos(forks, destPath)
+        break;
+      default:
+        console.warn("!!! No valid option specified");
+        break;
+    }
+    console.log(`[MSG] ${clonedRepos.length} repos cloned to ${destPath}`)
   } catch (err) {
     throw err;
   }
